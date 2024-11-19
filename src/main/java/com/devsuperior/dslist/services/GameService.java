1b24a40dd4f82315c0013a8901356840f6c3b304
marli-leite <marli.leite2301@outlook.com>
2024-11-19 15:29:31 -0300

    Move games on list

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -38,6 +38,5 @@ public class GameService {
 	public List<GameMinDTO> findByList(Long listId){
 		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
-		
 	}
 }
